afcbf3dca12121a332d2d506ae9f3467f7874048
Jonathan Christison <jochrist@redhat.com>
2019-03-06 15:53:15 +0000

    Hackz to get working with latest from redhat registry

--- fuse_online_config.sh
+++ fuse_online_config.sh
@@ -1,19 +1,19 @@
 # Tag for release. Update this before running release.sh
-git_fuse_online_install="1.5.8"
+git_fuse_online_install="1.5.8-prod"
 
 # Tags used for the productised images
-tag_server="1.2-4"
-tag_ui="1.2-2"
-tag_meta="1.2-3"
-tag_s2i="1.2-3"
-tag_upgrade="1.2-6"
-tag_operator="1.2-5"
+tag_server="latest"
+tag_ui="latest"
+tag_meta="latest"
+tag_s2i="latest"
+tag_upgrade="latest"
+tag_operator="latest"
 
 # Docker repository for productised images
 repository="fuse7"
 
 # Test:
-registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
+#registry="brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888"
 
 # Official:
-# registry="registry.access.redhat.com"
+ registry="registry.access.redhat.com"
